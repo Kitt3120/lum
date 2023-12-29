acfd147da7314bb7ad7bdc6c84a002dfb3804436
Torben Schweren <torben@schweren.dev>
2023-12-29 22:30:11 +0100

    Add config file (#14)
    * Implement config
    
     - Add dirs crate
     - Implement ConfigHandler
     - Implement custom error types
     - Implement config struct
     - Load config on startup
    
    * Use thiserror
    
     - Add thiserror crate
     - Refactor existing error types to use thiserror

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+mod config;
+
+pub const BOT_NAME: &str = "Lum";
+
 fn main() {
-    println!("Hello, world!");
+    let config_handler = config::ConfigHandler::new(BOT_NAME.to_lowercase().as_str());
+    let config = match config_handler.get_config() {
+        Ok(config) => config,
+        Err(err) => {
+            panic!("Error reading config file: {}", err);
+        }
+    };
+
+    println!("Config: {}", config);
 }
