98498dc43f7351935e11c591d698c650ae77c117
Torben Schweren <torben@schweren.dev>
2024-01-01 19:28:57 +0100

    Add Service Framework (#21)
    * Implement service framework
    
     - Make main async
     - Implement Status enum
     - Implement Priority enum
     - Implement ServiceInfo struct
     - Implement ServiceInternals trait
     - Implement Service trait
    
    * Bot library
    
     - Add fern crate
     - Add humantime crate
     - Add log crate
     - Implement Bot
     - Implement BotBuilder
     - Refactor config Display trait implementation
     - Implement library is_debug() function
     - Implement library run(Bot) function
     - Implement log module (log::setup(), log::is_set_up() and log::get_min_log_level())
     - Adapt main to new changes
    
    * WIP: Finish services framework
    
    Just a lot of refactoring and fixing. No time to describe all this now. Happy new year! :)
    
    * Finish services framework
    
    Too much to describe. It's done, that's it. This was one hell of a ride.

---
+++ src/lib.rs
@@ -0,0 +1,63 @@
+use crate::service::OverallStatus;
+use ::log::{error, info};
+use bot::Bot;
+use std::time::SystemTime;
+
+pub mod bot;
+pub mod config;
+pub mod log;
+pub mod service;
+
+pub fn is_debug() -> bool {
+    cfg!(debug_assertions)
+}
+
+pub async fn run(mut bot: Bot) {
+    if !log::is_set_up() {
+        eprintln!("Logger has not been set up!\n{} will exit.", bot.name);
+
+        return;
+    }
+
+    let now = SystemTime::now();
+
+    bot.start().await;
+
+    match now.elapsed() {
+        Ok(elapsed) => info!("Startup took {}ms", elapsed.as_millis()),
+        Err(error) => {
+            error!(
+                "Error getting elapsed startup time: {}\n{} will exit.",
+                error, bot.name
+            );
+
+            return;
+        }
+    };
+
+    if bot.service_manager.overall_status().await != OverallStatus::Healthy {
+        let status_tree = bot.service_manager.status_tree().await;
+
+        error!("{} is not healthy! Some essential services did not start up successfully. Please check the logs.\nService status tree:\n{}\n{} will exit.",
+        bot.name,
+        status_tree,
+        bot.name);
+        return;
+    }
+
+    info!("{} is alive", bot.name,);
+
+    //TODO: Add CLI commands
+    match tokio::signal::ctrl_c().await {
+        Ok(_) => {
+            info!("Received SIGINT, {} will now shut down", bot.name);
+        }
+        Err(error) => {
+            panic!("Error receiving SIGINT: {}\n{} will exit.", error, bot.name);
+        }
+    }
+
+    bot.stop().await;
+
+    info!("{} has shut down", bot.name);
+}
---
+++ src/log.rs
@@ -0,0 +1,50 @@
+use fern::colors::{Color, ColoredLevelConfig};
+use log::{LevelFilter, SetLoggerError};
+use std::{
+    io,
+    sync::atomic::{AtomicBool, Ordering},
+    time::SystemTime,
+};
+
+use crate::is_debug;
+
+static IS_LOGGER_SET_UP: AtomicBool = AtomicBool::new(false);
+
+pub fn is_set_up() -> bool {
+    IS_LOGGER_SET_UP.load(Ordering::Relaxed)
+}
+
+pub fn setup() -> Result<(), SetLoggerError> {
+    let colors = ColoredLevelConfig::new()
+        .info(Color::Green)
+        .debug(Color::Magenta)
+        .warn(Color::Yellow)
+        .error(Color::Red)
+        .trace(Color::Cyan);
+
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "[{} {: <25} {: <5}] {}",
+                humantime::format_rfc3339_seconds(SystemTime::now()),
+                record.target(),
+                colors.color(record.level()),
+                message
+            ))
+        })
+        .level(get_min_log_level())
+        .chain(io::stdout())
+        .apply()?;
+
+    IS_LOGGER_SET_UP.store(true, Ordering::Relaxed);
+
+    Ok(())
+}
+
+fn get_min_log_level() -> LevelFilter {
+    if is_debug() {
+        LevelFilter::Debug
+    } else {
+        LevelFilter::Info
+    }
+}
