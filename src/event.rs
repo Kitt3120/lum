c6ce2368b7c17627c02782a7f4472fd4f76fac05
Torben Schweren <torben@schweren.dev>
2024-02-10 16:38:25 +0100

    Event improvements
     - Add name attribute to Event<T>
     - Unify Channel and Closure subscribers by using an Enum
     - Propagate errors when dispatching events
     - Add error log when errors occur while dispatching events
     - Subscribers are now removed from an event when they run into an error while dispatching

--- src/event.rs
+++ src/event.rs
@@ -1,61 +1,98 @@
 use crate::service::BoxedError;
 use std::{fmt::Debug, sync::Arc};
 use tokio::sync::{
-    mpsc::{channel, Receiver, Sender},
+    mpsc::{channel, error::SendError, Receiver, Sender},
     Mutex,
 };
 
+pub enum Subscriber<T> {
+    Channel(Sender<Arc<T>>),
+    Closure(Box<dyn Fn(Arc<T>) -> Result<(), BoxedError> + Send + Sync>),
+}
+
+pub enum EventError<T> {
+    ChannelSend(SendError<Arc<T>>),
+    Closure(BoxedError),
+}
+
 pub struct Event<T> {
-    receivers: Mutex<Vec<Sender<Arc<T>>>>,
-    closures: Mutex<Vec<Box<dyn Fn(Arc<T>) -> Result<(), BoxedError> + Send>>>,
+    pub name: String,
+    subscribers: Mutex<Vec<Subscriber<T>>>,
 }
 
 impl<T> Event<T> {
-    pub fn new() -> Self {
+    pub fn new(name: &str) -> Self {
         Self {
-            receivers: Mutex::new(Vec::new()),
-            closures: Mutex::new(Vec::new()),
+            name: name.to_string(),
+            subscribers: Mutex::new(Vec::new()),
         }
     }
 
-    pub async fn get_receiver(&self, buffer: usize) -> Receiver<Arc<T>> {
+    pub async fn subscriber_count(&self) -> usize {
+        let subscribers = self.subscribers.lock().await;
+        subscribers.len()
+    }
+
+    pub async fn open_channel(&self, buffer: usize) -> Receiver<Arc<T>> {
         let (sender, receiver) = channel(buffer);
-        let mut subscribers = self.receivers.lock().await;
-        subscribers.push(sender);
+        let mut subscribers = self.subscribers.lock().await;
+        subscribers.push(Subscriber::Channel(sender));
         receiver
     }
 
-    pub async fn subscribe(&self, closure: impl Fn(Arc<T>) -> Result<(), BoxedError> + Send + 'static) {
-        let mut closures = self.closures.lock().await;
-        closures.push(Box::new(closure));
+    pub async fn subscribe(
+        &self,
+        closure: impl Fn(Arc<T>) -> Result<(), BoxedError> + Send + Sync + 'static,
+    ) {
+        let mut subscribers = self.subscribers.lock().await;
+        subscribers.push(Subscriber::Closure(Box::new(closure)));
     }
 
-    pub async fn dispatch(&self, data: T) {
-        let subscribers = self.receivers.lock().await;
+    pub async fn dispatch(&self, data: T) -> Result<(), Vec<EventError<T>>> {
+        let mut subscribers = self.subscribers.lock().await;
         let data = Arc::new(data);
 
-        for subscriber in subscribers.iter() {
+        let mut errors = Vec::new();
+        let mut subscribers_to_remove = Vec::new();
+
+        for (index, subscriber) in subscribers.iter().enumerate() {
             let data = Arc::clone(&data);
-            let result = subscriber.send(data).await;
-            if let Err(err) = result {
-                log::error!("Event failed to dispatch data to one of its receivers: {}", err);
+
+            match subscriber {
+                Subscriber::Channel(sender) => {
+                    let result = sender.send(data).await;
+                    if let Err(err) = result {
+                        log::error!("Event \"{}\" failed to dispatch data to receiver {}: {}. Receiver will be unregistered from event.", self.name, index, err);
+                        errors.push(EventError::ChannelSend(err));
+                        subscribers_to_remove.push(index);
+                    }
+                }
+                Subscriber::Closure(closure) => {
+                    let result = closure(data);
+                    if let Err(err) = result {
+                        log::error!("Event \"{}\" failed to dispatch data to closure {}: {}. Closure will be unregistered from event.", self.name, index, err);
+                        errors.push(EventError::Closure(err));
+                        subscribers_to_remove.push(index);
+                    }
+                }
             }
         }
 
-        let closures = self.closures.lock().await;
-        for closure in closures.iter() {
-            let data = Arc::clone(&data);
-            let result = closure(data);
-            if let Err(err) = result {
-                log::error!("Event failed to dispatch data to one of its closures: {}", err);
-            }
+        for index in subscribers_to_remove.into_iter().rev() {
+            subscribers.remove(index);
+        }
+
+        if errors.is_empty() {
+            Ok(())
+        } else {
+            Err(errors)
         }
     }
 }
 
 impl<T> Default for Event<T> {
     fn default() -> Self {
-        Self::new()
+        Self::new("Unnamed Event")
     }
 }
 
